b5a7ac51f68588fdd38b08eb72ee0ef37bc0911b
John Cunningham <aasolidgaming@gmail.com>
2025-11-30 22:46:01 -0500

    upgrade gradel, a bit of progress regarding scanner

--- client/app/src/main/java/com/marrow/client/SimpleBLEScanner.kt
+++ client/app/src/main/java/com/marrow/client/SimpleBLEScanner.kt
@@ -1,16 +1,41 @@
 package com.marrow.client
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
 import android.bluetooth.le.BluetoothLeScanner
+import android.bluetooth.le.ScanCallback
 import android.content.Context
+import androidx.annotation.RequiresPermission
 
-class SimpleBLEScanner
-    (private val context: Context, private val activity1: MainActivity) {
+class SimpleBLEScanner (private var context: Context, private var activity1: MainActivity)
+{
+    private var Scanner : BluetoothLeScanner? = null
+    private var BluetoothManager : BluetoothManager? = null
+    private var BluetoothAdapter : BluetoothAdapter? = null
 
-        private val Scanner : BluetoothLeScanner = BluetoothAdapter.getBluetoothLeScanner()
+    init {
+        BluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager?
+        // if null panic
+        BluetoothAdapter = BluetoothManager?.adapter
+        // if null panic
+        Scanner = BluetoothAdapter?.bluetoothLeScanner
+        // if null panic
 
-    fun StartScanning() {
+        @SuppressLint("MissingPermission")
+        StartScanning()
+    }
+
+    var test = object : ScanCallback() {
 
     }
+
+    @RequiresPermission(Manifest.permission.BLUETOOTH_SCAN)
+    fun StartScanning() {
+        Scanner?.startScan(test)
+    }
+
+
 
 }
