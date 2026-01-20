ddb3584369b4b3a94b60192c98b8c4b9b9a59e76
John Cunningham <aasolidgaming@gmail.com>
2026-01-19 23:17:45 -0500

    beginning of v2

---
+++ v2/primary/go.mod
@@ -0,0 +1,3 @@
+module john-v3/marrowv2-primary
+
+go 1.25.5
---
+++ v2/primary/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	fmt.Println("test")
+	fmt.Println(GetOutBoundIP())
+}
+
+func GetOutBoundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil { log.Fatal(err) }
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP
+}
+
