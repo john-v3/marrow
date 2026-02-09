7bde5caceebef0b032556b4d60fd298719ba6abb
John Cunningham <aasolidgaming@gmail.com>
2026-02-08 19:36:15 -0500

    intermediate work on qr code scanner implementation

--- v2/phoneclient/app/build.gradle.kts
+++ v2/phoneclient/app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.java.websocket)
     implementation(libs.androidx.appcompat)
+    implementation(libs.quickie.bundled)
+    implementation(libs.compose.theme.adapter)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- v2/phoneclient/app/src/main/java/com/example/phoneclient/MainActivity.kt
+++ v2/phoneclient/app/src/main/java/com/example/phoneclient/MainActivity.kt
@@ -1,21 +1,39 @@
 package com.example.phoneclient
 
 import WebSocketClient
+import android.bluetooth.le.ScanSettings
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.phoneclient.ui.theme.PhoneclientTheme
+import io.github.g00fy2.quickie.QRResult
+import io.github.g00fy2.quickie.QRResult.QRError
+import io.github.g00fy2.quickie.QRResult.QRMissingPermission
+import io.github.g00fy2.quickie.QRResult.QRSuccess
+import io.github.g00fy2.quickie.QRResult.QRUserCanceled
+import io.github.g00fy2.quickie.ScanCustomCode
+import io.github.g00fy2.quickie.ScanQRCode
+import io.github.g00fy2.quickie.config.BarcodeFormat
+import io.github.g00fy2.quickie.content.QRContent
 import java.net.URI
 
+
 class MainActivity : ComponentActivity() {
+
+    private var selectedBarcodeFormat = BarcodeFormat.FORMAT_ALL_FORMATS
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -50,7 +68,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-
 @Preview(showBackground = true, name = "test")
 @Composable
 fun GreetingPreview() {
@@ -58,3 +75,19 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+
+@Composable
+fun ButtonBeginScan(modifier : Modifier = Modifier) {
+    val scanQRCodeLauncher = rememberLauncherForActivityResult(ScanQRCode()) {
+        result -> {
+         qRResult ->
+        }
+    }
+
+    Button(onClick = { scanQRCodeLauncher.launch(null)}) {
+        Text("Scan QR Code")
+    }
+}
+
+
+fun
